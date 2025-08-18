dbf40bdc585c4a662fe518a07fa4c7ad6ada97b5
ColeOBalle <cole.oballe@gmail.com>
2025-08-17 17:47:07 -0700

    home page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,73 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Icon from '@mui/material/Icon'
+import { Button, createTheme } from "@mui/material";
+import './page.css'
+
+const buttonTheme = createTheme({
+  typography: {
+
+  }
+});
 
 export default function Home() 
 {
   return (
-    <>
-    </>
+    <main>
+
+      <section id="aboutUs">
+        <div id="titleDiv">
+          <h1 id="homeTitle"><Icon fontSize="inherit">flare</Icon> About Us <Icon fontSize="inherit">flare</Icon></h1>
+          <h2 id="introParagraph">We’re a small team of college students passionate about creating story-driven games that inspire players to make a difference in the world.</h2>
+        </div>
+        <div className="imgDiv logo">
+          <Image
+            src="/logo.png"
+            alt="DelightedGames Logo"
+            fill
+            objectFit="contain"
+          />
+        </div>
+      </section>
+
+      <h1>What We're Working On:</h1>
+
+      <section id="projSection">
+        <div className="imgDiv gameImg">
+          <Image
+            src="/Landed Updated Logo.png"
+            alt="Landed logo"
+            fill
+            objectFit="contain"
+          />
+        </div>
+        <div className="descDiv">
+          <h3>Landed</h3>
+          <p>Our first major project, Landed, invites you to navigate a world on the brink of demise—where every choice shapes the fate of those around you.</p>
+          <Button variant="contained" sx={{
+            fontSize: {
+              xs: "0.75rem",
+              sm: "1rem",
+              md: "1.5rem",
+              lg: "1.75rem"
+            },
+            padding: {
+              xs: "10px",
+              sm: "10px 15px",
+              md: "10px 20px",
+              lg: "10px 40px"
+            }
+            }}>Learn More
+          </Button>
+          <div></div>
+        </div>
+      </section>
+
+      <section id="meetTheTeam">
+            <div id="teamGrid">
+              
+            </div>
+      </section>
+    </main>
   )
 }
