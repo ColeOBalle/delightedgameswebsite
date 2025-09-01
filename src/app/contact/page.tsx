fb8c149482264195640e0d79445b246e87f50057
ColeOBalle <cole.oballe@gmail.com>
2025-08-31 17:52:36 -0700

    contact page started

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,44 @@
+import { Button, Icon } from '@mui/material'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import styles from './contact.module.css'
 
 export default async function ContactLayout() {
     return (
-        <>
-        </>
+        <main>
+            <h1 className={styles.h1+" "+styles.title}>Follow Us!</h1>
+            <section className={styles.socialsLinkGroup}>
+                <Button variant='outlined' size='large' startIcon={<InstagramIcon className={styles.buttonIcon}/>} sx={{
+                    fontSize: {
+                    sm: "1rem",
+                    md: "1.5rem",
+                    lg: "1.75rem"
+                    },
+                    padding: {
+                    sm: "10px 15px",
+                    md: "10px 20px",
+                    lg: "10px 40px"
+                    },
+                    width: 400,
+                    marginBottom: "25px",
+                    textTransform: "none"
+                    }}>@delighted.games</Button>
+                <Button variant='outlined' size='large' startIcon={<YouTubeIcon className={styles.buttonIcon}/>} sx={{
+                    fontSize: {
+                    sm: "1rem",
+                    md: "1.5rem",
+                    lg: "1.75rem"
+                    },
+                    padding: {
+                    sm: "10px 15px",
+                    md: "10px 20px",
+                    lg: "10px 40px"
+                    },
+                    width: 400,
+                    marginBottom: "25px",
+                    textTransform: "none"
+                    }}>@delightedgames</Button>
+            </section>
+        </main>
     )
 }
