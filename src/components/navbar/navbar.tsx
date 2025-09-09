49a72b87b341fa03d8de68173c65af3e59172b56
ColeOBalle <cole.oballe@gmail.com>
2025-09-09 12:57:22 -0700

    hamburger button

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Icon from '@mui/material/Icon'
+import { useState } from 'react';
+import { useWindowWidth } from '@react-hook/window-size';
+import { Spin as Hamburger } from 'hamburger-react';
 import '@/app/globals.css';
 import styles from './navbar.module.css';
 import { defaultConfig } from 'next/dist/server/config-shared';
@@ -19,10 +22,10 @@ function Logo() {
                 objectFit='contain'
             />
         </Link>
-    )
+    );
 }
 
-export default function NavBar() {
+function StandardNavBar() {
     return (
         <header className={styles.header}>
             <section className={styles.logoSection}>
@@ -39,6 +42,36 @@ export default function NavBar() {
                 <Link href="/contact" className={styles.navOption}>Contact</Link>
             </nav>
         </header>
+    );
+}
 
-    )
+function HamburgerNavBar(){
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    
+    return (
+        <header className={styles.header}>
+            <section className={styles.logoSection}>
+                <figure className={styles.logo}>
+                    <Logo />
+                </figure>
+                <h1 className={styles.logoText}>Delighted Games</h1>
+            </section>
+            <nav className={styles.nav}>
+                <Hamburger direction='left' size={20} toggled={hamburgerOpen} toggle={setHamburgerOpen}/>
+            </nav>
+        </header>
+    );
+}
+
+export default function NavBar() {
+    const screenWidth = useWindowWidth();
+
+    if(screenWidth >= 600){
+        // width screen = standard nav bar
+        return (<StandardNavBar/>);
+
+    } else {
+        // small screen = hamburger menu
+        return (<HamburgerNavBar/>);
+    }
 }
