76d5b672984bc20d945cc1cbfc9295a46896df14
ColeOBalle <cole.oballe@gmail.com>
2025-09-09 13:47:11 -0700

    drop down nav menu for mobile users

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,9 +7,11 @@ import Icon from '@mui/material/Icon'
 import { useState } from 'react';
 import { useWindowWidth } from '@react-hook/window-size';
 import { Spin as Hamburger } from 'hamburger-react';
+import { AnimatePresence, motion } from 'framer-motion';
 import '@/app/globals.css';
 import styles from './navbar.module.css';
 import { defaultConfig } from 'next/dist/server/config-shared';
+import { duration } from '@mui/material';
 
 function Logo() {
     return (
@@ -47,7 +49,21 @@ function StandardNavBar() {
 
 function HamburgerNavBar(){
     const [hamburgerOpen, setHamburgerOpen] = useState(false);
-    
+    const routes = [
+        {
+            title: 'Home',
+            href: '/'
+        },
+        {
+            title: 'Games',
+            href: '/games'
+        },
+        {
+            title: 'Contact',
+            href: '/contact'
+        }
+    ];
+
     return (
         <header className={styles.header}>
             <section className={styles.logoSection}>
@@ -58,6 +74,42 @@ function HamburgerNavBar(){
             </section>
             <nav className={styles.nav}>
                 <Hamburger direction='left' size={20} toggled={hamburgerOpen} toggle={setHamburgerOpen}/>
+                {hamburgerOpen && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2 }}
+                        className={styles.dropdown}
+                    >
+                        <ul className={styles.dropdownUL}>
+                            {routes.map((route, index) => {        
+                                return(
+                                    <motion.li
+                                        initial={{ scale: 0, opacity: 0 }}
+                                        animate={{ scale: 1, opacity: 1 }}
+                                        transition={{
+                                            type: "spring",
+                                            stiffness: 260,
+                                            damping: 20,
+                                            delay: 0.1 + index / 10
+                                        }}
+                                        key={route.title}
+                                        className={styles.dropdownItem}
+                                    >
+                                        <a
+                                            href={route.href}
+                                            onClick={() => setHamburgerOpen((prev) => !prev)}
+                                            className={styles.dropdownItemLink}
+                                        >
+                                            <p>{route.title}</p>
+                                        </a>
+                                    </motion.li>
+                                );
+                            })}
+                        </ul>
+                    </motion.div>
+                )}
             </nav>
         </header>
     );
